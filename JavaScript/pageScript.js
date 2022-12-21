245c904806e6b673dfbf1ee7ef5fcc705903eb7b
alisonViana <alisonviana.c@gmail.com>
2022-12-20 22:01:48 -0300

    feat: add javascript lesson 1

---
+++ JavaScript/pageScript.js
@@ -0,0 +1,10 @@
+// Aula 1 - Hello World
+
+alert("Hello World! This is my first JS code!");
+
+count = 0;
+
+function buttonCount() {
+	count++;
+	document.getElementById("count").innerHTML = count;
+}
